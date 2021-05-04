aeb321324861888b07741692dea01ce36382e2dc
Thisara Hettikankanam <thisarayashodagcc@gmail.com>
2021-05-04 22:01:28 +0530

    update qr

---
+++ app/src/main/java/com/example/qrscanner/Scanner_.kt
@@ -0,0 +1,11 @@
+package com.example.qrscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scanner_ : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner_)
+    }
+}
